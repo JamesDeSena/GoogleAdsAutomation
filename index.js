500a22e128d04b4b7b2dc862bfa0926ade1f5445
James De Sena <jamesdesena27@gmail.com>
2024-12-17 08:46:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -60,16 +60,6 @@ const AM = schedule.scheduleJob(rule1, () => {
   console.log("Scheduled pacing report sent at 7 AM PST California/Irvine.");
 });
 
-const rule2 = new schedule.RecurrenceRule();
-rule2.hour = 19;
-rule2.minute = 0;
-rule2.tz = 'America/Los_Angeles';
-
-const PM = schedule.scheduleJob(rule2, () => {
-  sendFinalPacingReportToAirtable();
-  console.log("Scheduled pacing report sent at 7 PM PST California/Irvine.");
-});
-
 const dailyReportJob = schedule.scheduleJob(rule1, () => {
   fetchReportDataDaily();
   console.log("Scheduled daily report sent at 7 AM PST California/Irvine.");
@@ -80,7 +70,22 @@ const weeklyReportJob = schedule.scheduleJob(rule1, () => {
   console.log("Scheduled weekly report sent at 7 AM PST California/Irvine.");
 });
 
-const monthlyReportJob = schedule.scheduleJob(rule1, () => {
+const AMmonthlyReportJob = schedule.scheduleJob(rule1, () => {
+  sendFinalMonthlyReportToAirtable();
+  console.log("Scheduled monthly report sent at 7 AM PST California/Irvine.");
+});
+
+const rule2 = new schedule.RecurrenceRule();
+rule2.hour = 19;
+rule2.minute = 0;
+rule2.tz = 'America/Los_Angeles';
+
+const PM = schedule.scheduleJob(rule2, () => {
+  sendFinalPacingReportToAirtable();
+  console.log("Scheduled pacing report sent at 7 PM PST California/Irvine.");
+});
+
+const PMmonthlyReportJob = schedule.scheduleJob(rule2, () => {
   sendFinalMonthlyReportToAirtable();
   console.log("Scheduled monthly report sent at 7 AM PST California/Irvine.");
 });
