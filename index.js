a9247ad5912bee8e172ea3bbd79bc0580956d17f
James De Sena <jamesdesena27@gmail.com>
2024-12-19 07:29:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,44 +50,44 @@ app.use("/", (req, res) => {
 
 pingRenderApp();
 
+const executeConcurrentJobs = async (jobs, jobName) => {
+  console.log(`Executing ${jobName} jobs concurrently.`);
+  try {
+    await Promise.all(jobs.map(job => job()));
+    console.log(`${jobName} jobs completed successfully.`);
+  } catch (error) {
+    console.error(`Error in ${jobName} jobs:`, error);
+  }
+};
+
 const rule1 = new schedule.RecurrenceRule();
 rule1.hour = 7;
 rule1.minute = 0;
 rule1.tz = 'America/Los_Angeles';
-
-const AM = schedule.scheduleJob(rule1, () => {
-  sendFinalPacingReportToAirtable();
-  console.log("Scheduled pacing report sent at 7 AM PST California/Irvine.");
-});
-
-const dailyReportJob = schedule.scheduleJob(rule1, () => {
-  fetchReportDataDaily();
-  console.log("Scheduled daily report sent at 7 AM PST California/Irvine.");
-});
-
-const weeklyReportJob = schedule.scheduleJob(rule1, () => {
-  sendFinalWeeklyReportToAirtable();
-  console.log("Scheduled weekly report sent at 7 AM PST California/Irvine.");
-});
-
-const AMmonthlyReportJob = schedule.scheduleJob(rule1, () => {
-  sendFinalMonthlyReportToAirtable();
-  console.log("Scheduled monthly report sent at 7 AM PST California/Irvine.");
-});
 
 const rule2 = new schedule.RecurrenceRule();
 rule2.hour = 19;
 rule2.minute = 0;
 rule2.tz = 'America/Los_Angeles';
 
-const PM = schedule.scheduleJob(rule2, () => {
-  sendFinalPacingReportToAirtable();
-  console.log("Scheduled pacing report sent at 7 PM PST California/Irvine.");
+const morningJobs = [
+  sendFinalPacingReportToAirtable,
+  fetchReportDataDaily,
+  sendFinalWeeklyReportToAirtable,
+  sendFinalMonthlyReportToAirtable
+];
+
+const eveningJobs = [
+  sendFinalPacingReportToAirtable,
+  sendFinalMonthlyReportToAirtable
+];
+
+schedule.scheduleJob(rule1, () => {
+  executeConcurrentJobs(morningJobs, "Morning");
 });
 
-const PMmonthlyReportJob = schedule.scheduleJob(rule2, () => {
-  sendFinalMonthlyReportToAirtable();
-  console.log("Scheduled monthly report sent at 7 AM PST California/Irvine.");
+schedule.scheduleJob(rule2, () => {
+  executeConcurrentJobs(eveningJobs, "Evening");
 });
 
 // schedule.scheduleJob('* * * * *', () => {
